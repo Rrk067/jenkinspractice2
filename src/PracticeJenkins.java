24f9a4ff3d82df5634c78ddc05aedd10500fea05
Rrk067 <rrk.224@gmail.com>
2021-05-27 06:19:09 -0400

    Added a print statement

--- src/PracticeJenkins.java
+++ src/PracticeJenkins.java
@@ -2,6 +2,7 @@ public class PracticeJenkins {
     public static void main(String[] args) {
         System.out.println("hi jenkins");
         System.out.println("Hello ray");
+        System.out.println("feature branch added");
     }
 
 }
